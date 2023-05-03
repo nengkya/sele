3db3c059f55d8b9b60bb3d80f343b3245fb0d950
nengkya <nengkya@yahoo.com>
2023-05-03 13:59:18 +0700

    export doesnt work. dataframe works

--- sele.py
+++ sele.py
@@ -1,6 +1,9 @@
 import os
+import pandas as pd
+import csv
 import fnmatch
 
+from bs4 import BeautifulSoup, SoupStrainer
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -33,10 +36,43 @@ class Se:
 
 		driver.implicitly_wait(1)
 
-		WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/form[1]/div[3]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[7]/td[2]/button[3]"))).click()
+		WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/form[1]/div[3]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[7]/td[2]/button[1]"))).click()
+
 		action.pause(5)
 		#perform the operation
 		action.perform()
+
+		soup = BeautifulSoup(driver.page_source, features = "html.parser")
+
+		df_pandas=pd.read_html(driver.page_source, attrs={'class':'table-bordered'},flavor='html5lib')
+
+
+		####################
+		#data of csv file
+		for table in range(1, len(df_pandas)):
+			rows = df_pandas[table].values.tolist()
+
+			#name of csv file
+			a_tab = SoupStrainer('li',{'class': 'active'})
+
+			soup1 = BeautifulSoup(driver.page_source, features = "html.parser", parseOnlyThese = a_tab)
+
+			a = soup1.find('a')
+
+			filename = hs_code.strip() + ' ' + str(a.string) + " table " + str(table) + ".csv"
+				
+			#writing to csv file 
+			with open(filename, 'w') as csvfile: 
+				#creating a csv writer object 
+				csvwriter = csv.writer(csvfile) 
+
+				if table == 2:
+					csvwriter.writerow(list(df_pandas[table]))
+					
+				#writing the data rows
+				for i in range(0, len(rows)):
+					csvwriter.writerow(rows[i])
+		############
 
 		for file in os.listdir('.'):
 			if fnmatch.fnmatch(file, 'Statistic*.txt'):
